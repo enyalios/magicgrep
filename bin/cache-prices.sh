77ae1f2b0baafdd9d4d75821646b004d3ed651ed
Paul Guder <github@enyalios.net>
2023-06-30 18:22:54 -0500

    Making the cache-prices script kill other instances.

--- bin/cache-prices.sh
+++ bin/cache-prices.sh
@@ -3,6 +3,8 @@
 # this script caches the price of the 1/7 least recently updated cards in the db
 # run everyday it will cache prices for everything over the course of a week
 
+# kill other runs of this script
+pgrep -f ${BASH_SOURCE[0]} | grep -v $$ | xargs -r kill
 cd `dirname $0`/..
 echo 'select distinct price_name from cards order by price_updated limit (select count(distinct price_name) / 7 from cards);' \
     | sqlite3 db/magic.db \
